c2e476a009316c89b36288f09b8faf5d3e82c5e2
Yael <josue.yael2703@gmail.com>
2026-05-11 09:07:18 -0600

    Se modifico slider.js para que no exista imagen default, se elimino la imagen de fondo y se agrego un mensaje que indique que no hay imagen disponible. Ademas se modifico el archivo usuarios.php para que en lugar de traer el codigo de db.php traiga el codigo de dbpgs.php, esto con el fin de conectar a una base de datos PostgreSQL en lugar de MySQL.

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -1,16 +1,49 @@
-let id_actual = 1; // ID inicial
+let id_actual = 0;
+
+// CARGAR PRIMERA IMAGEN
+window.onload = function () {
+    cargarInicial();
+};
+
+function cargarInicial() {
+
+    fetch("slider.php?action=mostrar_inicial")
+    .then(res => res.json())
+    .then(data => {
+
+        if (data.status === "ok") {
+
+            id_actual = data.id;
+
+            document.getElementById("imagenSlider").src = data.ruta;
+            document.getElementById("imagenSlider").style.display = "block";
+
+            document.getElementById("sinImagenes").style.display = "none";
+
+        } else {
+
+            document.getElementById("imagenSlider").style.display = "none";
+            document.getElementById("sinImagenes").style.display = "block";
+        }
+    });
+}
 
 function cargarImagen(direccion) {
+
     fetch(`slider.php?action=mostrar&id=${id_actual}&dir=${direccion}`)
     .then(res => res.json())
     .then(data => {
 
         if (data.status === "ok") {
+
             id_actual = data.id;
 
             document.getElementById("imagenSlider").src = data.ruta;
+
         } else {
-            console.log("No hay más imágenes");
+
+            document.getElementById("imagenSlider").style.display = "none";
+            document.getElementById("sinImagenes").style.display = "block";
         }
 
     })
